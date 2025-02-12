db71fb5e12e5088b46efc8fc7ad04c1d8aeba37b
André Silva <andreoew@hotmail.com>
2025-02-11 23:23:27 -0300

    fix: cors errors

--- email-toy/src/server.ts
+++ email-toy/src/server.ts
@@ -4,16 +4,16 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { contactRoutes } from "./routes/contacts";
 import { errorHandler } from "./error-handler";
 import { env } from "./env";
-// import { getContacts } from "./routes/promo/get-contacts";
-// import { createContact } from "./routes/promo/create-contact";
-// import { getDateValidityPromo } from "./routes/promo/get-validity-promo";
-// import { createPromoValidity } from "./routes/promo/create-validity-promo";
+import { getContacts } from "./routes/promo/get-contacts";
+import { createContact } from "./routes/promo/create-contact";
+import { getDateValidityPromo } from "./routes/promo/get-validity-promo";
+import { createPromoValidity } from "./routes/promo/create-validity-promo";
 
 
 const app = fastify()
 
 app.register(cors, {
-  origin: ['https://www.toyotan.com.br'],
+  origin: ['*'],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],  
 })
@@ -24,10 +24,10 @@ app.setSerializerCompiler(serializerCompiler);
 app.setErrorHandler(errorHandler)
 
 app.register(contactRoutes)
-// app.register(createContact)
-// app.register(getContacts)
-// app.register(getDateValidityPromo)
-// app.register(createPromoValidity)
+app.register(createContact)
+app.register(getContacts)
+app.register(getDateValidityPromo)
+app.register(createPromoValidity)
 
 app.listen({
   port: env.PORT,
