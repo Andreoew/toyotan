27d884b341cb65ee13260d2df7a9f0b55d81555c
André Silva <andreoew@hotmail.com>
2025-02-11 23:19:23 -0300

    fix: cors erros

--- email-toy/src/server.ts
+++ email-toy/src/server.ts
@@ -4,10 +4,10 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { contactRoutes } from "./routes/contacts";
 import { errorHandler } from "./error-handler";
 import { env } from "./env";
-import { getContacts } from "./routes/promo/get-contacts";
-import { createContact } from "./routes/promo/create-contact";
-import { getDateValidityPromo } from "./routes/promo/get-validity-promo";
-import { createPromoValidity } from "./routes/promo/create-validity-promo";
+// import { getContacts } from "./routes/promo/get-contacts";
+// import { createContact } from "./routes/promo/create-contact";
+// import { getDateValidityPromo } from "./routes/promo/get-validity-promo";
+// import { createPromoValidity } from "./routes/promo/create-validity-promo";
 
 
 const app = fastify()
@@ -24,10 +24,10 @@ app.setSerializerCompiler(serializerCompiler);
 app.setErrorHandler(errorHandler)
 
 app.register(contactRoutes)
-app.register(createContact)
-app.register(getContacts)
-app.register(getDateValidityPromo)
-app.register(createPromoValidity)
+// app.register(createContact)
+// app.register(getContacts)
+// app.register(getDateValidityPromo)
+// app.register(createPromoValidity)
 
 app.listen({
   port: env.PORT,
